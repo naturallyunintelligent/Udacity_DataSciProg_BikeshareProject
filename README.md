ddd6d077402c08192e57b3b91ba572a2d451f94b
Darren Steele <darren.steele@five.ai>
2022-02-25 11:28:04 +0000

    bikeshare project - refactor so this repo is only the project

---
+++ README.md
@@ -0,0 +1,61 @@
+# Useful reference notes:
+
+# Date & Time:
+# https://realpython.com/python-datetime/
+# https://www.kite.com/python/answers/how-to-convert-between-month-name-and-month-number-in-python
+# https://stackoverflow.com/questions/23294658/asking-the-user-for-input-until-they-give-a-valid-response
+# https://docs.python.org/3/library/datetime.html#datetime.date.weekday
+
+# List Comprehensions:
+# https://realpython.com/list-comprehension-python/#using-list-comprehensions
+
+# Pandas quick reference:
+# https://pandas.pydata.org/pandas-docs/stable/user_guide/10min.html
+
+# regex:
+# https://stackoverflow.com/questions/43281715/regex-that-matches-all-week-days-name-in-a-string
+# https://pythex.org/
+
+# or more simply:
+# https://realpython.com/python-packages/#parse-for-matching-strings
+
+# Removing braces displayed in fstrings:
+# https://stackoverflow.com/questions/42756537/f-string-syntax-for-unpacking-a-list-with-brace-suppression
+
+Type annotations etc:
+https://realpython.com/python-type-checking/
+
+# Other
+# https://stackoverflow.com/questions/39604271/conda-environments-not-showing-up-in-jupyter-notebook
+
+
+to add somewhere else:
+https://github.com/sindresorhus/pure
+https://vim-adventures.com/
+https://github.com/veltman/clmystery
+https://docs.microsoft.com/en-us/windows/wsl/about
+https://www.hackerrank.com/challenges/matching-anything-but-new-line/problem?isFullScreen=true&h_r=next-challenge&h_v=zen
+google coding challenge thingy that popped up?
+medium towards data science - deforestation project mentioned on RP podcast
+
+Anki: move anything useful from cherrytree into anki?
+https://docs.ankiweb.net/getting-started.html
+https://ankiweb.net/shared/decks/
+https://ankiweb.net/shared/decks/python
+https://ankiweb.net/shared/info/1111933094
+
+Anki cards:
+PyCharm shortcuts
+To quickly evaluate the value of any expression while debugging the program, hold Alt (Windows and Linux) or Alt(Option) (macOS) and click this expression to see its value and calculate it, call a method, and so on.
+
+
+    # useful month conversion:
+    # month_number = "3"
+    # datetime_object = datetime.datetime.strptime(month_number, "%m")
+    # month_name = datetime_object.strftime("%b")
+    # print(month_name)
+    # OUTPUT
+    # Mar
+    #
+    # full_month_name = datetime_object.strftime("%B")
+    # print(full_month_name)
--- src/bikeshare_project.py
+++ src/bikeshare_project.py
@@ -1,16 +1,44 @@
+import re
 import time
-import parse
+from parse import *
 import datetime
 import pandas as pd
 import numpy as np
 
-
 # TODO: regex in user inputs
 # TODO: remove whitespace in user inputs
+# TODO: lookup Rich and Textual, re improving the terminal UI
 
-CITY_DATA = { 'chicago': 'chicago.csv',
-              'new york city': 'new_york_city.csv',
-              'washington': 'washington.csv' }
+CITY_DATA = {
+    'chicago': 'chicago.csv',
+    'new york city': 'new_york_city.csv',
+    'washington': 'washington.csv'
+}
+
+WEEKDAYS = [
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thursday',
+    'Friday',
+    'Saturday',
+    'Sunday',
+]
+
+MONTHNAMES = [
+        'January',
+        'February',
+        'March',
+        'April',
+        'May',
+        'June',
+        'July',
+        'August',
+        'September',
+        'October',
+        'November',
+        'December',
+]
 
 
 def greeting() -> None:
@@ -30,18 +58,14 @@ def greeting() -> None:
 
 def get_city() -> str:
     """
-    Takes input from the user to choose a city to analyse
-    :return city: the city chosen for analysis
+    Takes input from the user to select a city for analysis
+    :return city: The city chosen for analysis
     """
     # get user input for city, allowing for future expansion of data to further cities
     while True:
         available_cities = [city for city in CITY_DATA.keys()]
         try:
             city = str(input(f'\nWhich city would you like to analyse? '
-                             # f'Data currently available for {str(available_cities)}\n'
-                             # f'Data currently available for {str(available_cities)[1:-2]}\n'
-                             # f'Data currently available for {str(available_cities),}\n' 
-                             # f'Data currently available for {*available_cities,}\n'
                              f'Data currently available for {", ".join(str(city) for city in available_cities)}\n'
                              ))
             city = city.lower()
@@ -55,10 +79,10 @@ def get_city() -> str:
                 "nyc": "new york city",
             }
             if city in typos.keys():
-                print(f"Correcting typo: {city} >>> {typos[city]}")
+                print(f"City identified: {city} >>> {typos[city]}")
                 city = typos[city]
             if city not in available_cities:
-                print(f"Apologies, data currently available for {available_cities,}")
+                print(f"Apologies, data only currently available for {available_cities,}")
                 continue
         except ValueError:
             print(f"Apologies, I didn't understand that. Data currently available for {available_cities,}")
@@ -83,24 +107,11 @@ def get_month() -> (int, str):
                     'TypeError: Apologies, only strings handled, please enter a calendar month in English, e.g. January, '
                     'February, March, April, May, June, July, September, October, November or December')
                 continue
-            full_monthnames = [
-                "January",
-                "February",
-                "March",
-                "April",
-                "May",
-                "June",
-                "July",
-                "August",
-                "September",
-                "October",
-                "November",
-                "December",
-                ]
-            abb_monthnames = [name.lower()[:3] for name in full_monthnames]
-            lowercase_monthnames = [name.lower() for name in full_monthnames]
 
-            if chosen_month in lowercase_monthnames:
+            abb_monthnames = [name.lower()[:3] for name in MONTHNAMES]
+            # lowercase_monthnames = [name.lower() for name in MONTHNAMES]
+
+            if chosen_month in [name.lower() for name in MONTHNAMES]:
                 chosen_month = datetime.datetime.strptime(chosen_month, "%B")
                 chosen_month = chosen_month.strftime("%b").lower()
             if chosen_month in abb_monthnames:
@@ -127,7 +138,7 @@ def get_month() -> (int, str):
             #print("Attempted input")
 
     month_number = chosen_month.month
-    month_name = full_monthnames[month_number-1]
+    month_name = MONTHNAMES[month_number-1]
 
     return month_number, month_name
 
@@ -140,33 +151,33 @@ def get_day() -> (int, str):
     while True:
         try:
             chosen_day = str(input('\nWhich day would you like to analyse?\n')).lower()
+            if chosen_day[:3] in [day.lower()[:3] for day in WEEKDAYS]:
+                break
+            else:
+                continue
         except:
             print('Apologies, not understood, please enter a day of the week in English, \n '
                   'e.g. Monday, Tuesday, Wednesday, Thursday, Friday, Saturday or Sunday')
             continue
-        else:
-            break
+        # else:
+        #     break
 
-    display_daynames = [
-        'Monday',
-        'Tuesday',
-        'Wednesday',
-        'Thursday',
-        'Friday',
-        'Saturday',
-        'Sunday', ]
-    abb_daynames = [name.lower()[:3] for name in display_daynames]
-    lowercase_daynames = [name.lower() for name in display_daynames]
 
-    if chosen_day in display_daynames:
-        day_index = display_daynames.index(chosen_day)
-    if chosen_day in abb_daynames:
-        day_index = abb_daynames.index(chosen_day)
-    elif chosen_day in lowercase_daynames:
-        day_index =lowercase_daynames.index(chosen_day)
+    # abb_daynames = [name.lower()[:3] for name in WEEKDAYS]
+    # lowercase_daynames = [name.lower() for name in WEEKDAYS]
 
+    # if "day" in chosen_day:
+    #     chosen_day = search('{}day', chosen_day)[0]
+
+    # if chosen_day in display_daynames:
+    #     day_index = display_daynames.index(chosen_day)
+    # if chosen_day[:3] in [day.lower()[:3] for day in WEEKDAYS]:
+    #     day_index = [day.lower()[:3] for day in WEEKDAYS].index(chosen_day[:3])
+    # # elif chosen_day in lowercase_daynames:
+    #     day_index =lowercase_daynames.index(chosen_day)
+    day_index = [day.lower()[:3] for day in WEEKDAYS].index(chosen_day[:3])
     day_number = day_index + 1
-    day_name = display_daynames[day_index]
+    day_name = WEEKDAYS[day_index]
     print(f"Day selected: {day_name}, (weekday: {day_number})")
     return day_number, day_name
 
@@ -210,9 +221,13 @@ def load_data(user_filters):
     #print(type(df['Start Time'][0]))
     df['Start Time'] = pd.to_datetime(df['Start Time'])
     #print(type(df['Start Time'][0]))
+    df['End Time'] = pd.to_datetime(df['End Time'])
 
     df['month'] = df['Start Time'].dt.month
     df['day_of_week'] = df['Start Time'].dt.weekday
+    df['start_hour'] = df['Start Time'].dt.hour
+    df['journey'] = df['Start Station']+df['End Station']
+    df['Journey Time'] = df['End Time'] - df['Start Time']
 
     print(f"{df['month'][0]}, {type(df['month'][0])}")
     print(f"{user_filters['month_number']}, {type(user_filters['month_number'])}")
@@ -223,95 +238,97 @@ def load_data(user_filters):
     return df
 
 
-def time_stats(df):
+def time_stats(df, user_filters):
     """Displays statistics on the most frequent times of travel."""
-
     print('\nCalculating The Most Frequent Times of Travel for your chosen filters...\n'
-          'Chosen Filters: ')
+          'Chosen Filters:\n'
+          f'City - {user_filters["city"]}\n'
+          f'Month - {user_filters["month_name"]}\n'
+          f'Day of the week - {user_filters["day_name"]}\n'
+          )
     start_time = time.time()
     stats = df.describe()
 
     # TODO:intelligently show/don't show when filtered (but check rubric for requirements)
     # display the most common month
-    #modes = df.mode()
-    monthnames = [
-        "January",
-        "February",
-        "March",
-        "April",
-        "May",
-        "June",
-        "July",
-        "August",
-        "September",
-        "October",
-        "November",
-        "December",
-    ]
-
-    common_month = df.mode()['month'][0]
-    print(f"The most common month in the filtered data is {monthnames[common_month-1]}")
+    common_month = int(df.mode()['month'][0])
+    print(f"The most common month in the filtered data is {MONTHNAMES[common_month-1]}")
 
     # display the most common day of week
-    common_day = df.mode()['day_of_week'][0]
-    print(f"The most popular day of the week in the filtered data is {monthnames[common_month - 1]}")
+    common_day = int(df.mode()['day_of_week'][0])
+    print(f"The most popular day of the week in the filtered data is {WEEKDAYS[common_day - 1]}")
 
     # display the most common start hour
-
+    common_hour = int(df.mode()['start_hour'][0])
+    print(f"The most popular hour in the filtered data is {common_hour}")
 
     print("\nThis took %s seconds." % (time.time() - start_time))
     print('-'*40)
 
 
-def station_stats(df):
+def station_stats(df, user_filters):
     """Displays statistics on the most popular stations and trip."""
 
     print('\nCalculating The Most Popular Stations and Trip...\n')
     start_time = time.time()
 
     # display most commonly used start station
-
+    common_start_station = df.mode()['Start Station'][0]
+    print(f"The most popular Start Station in the filtered data is {common_start_station}")
 
     # display most commonly used end station
-
+    common_end_station = df.mode()['End Station'][0]
+    print(f"The most popular End Station in the filtered data is {common_end_station}")
 
     # display most frequent combination of start station and end station trip
-
+    common_journey = df.mode()['journey'][0]
+    print(f"The most popular journey (station combo) in the filtered data is {common_journey}")
 
     print("\nThis took %s seconds." % (time.time() - start_time))
     print('-'*40)
 
 
-def trip_duration_stats(df):
+def trip_duration_stats(df, user_filters):
     """Displays statistics on the total and average trip duration."""
 
     print('\nCalculating Trip Duration...\n')
     start_time = time.time()
 
     # display total travel time
-
+    total_travel_time = df["Journey Time"].sum()
+    print(f"The total travel time for the filtered data is {total_travel_time}")
 
     # display mean travel time
-
+    mean_travel_time = df["Journey Time"].mean()
+    print(f"The mean travel time for the filtered data is {mean_travel_time}")
 
     print("\nThis took %s seconds." % (time.time() - start_time))
     print('-'*40)
 
 
-def user_stats(df):
+def user_stats(df, user_filters):
     """Displays statistics on bikeshare users."""
 
-    print('\nCalculating User Stats...\n')
+    print('\nCalculating User Stats for your filters...\n')
     start_time = time.time()
 
     # Display counts of user types
-
+    #user_type_describe = df["User Type"].describe()
+    #print(user_type_describe)
+    user_type_counts = df["User Type"].value_counts()
+    print(user_type_counts)
 
     # Display counts of gender
-
+    gender_type_counts = df["Gender"].value_counts()
+    print(gender_type_counts)
 
     # Display earliest, most recent, and most common year of birth
-
+    earliest_dob = df["Birth Year"].min()
+    print(earliest_dob)
+    latest_dob = df["Birth Year"].max()
+    print(latest_dob)
+    common_dob = df.mode()["Birth Year"][0]
+    print(common_dob)
 
     print("\nThis took %s seconds." % (time.time() - start_time))
     print('-'*40)
@@ -332,6 +349,20 @@ def main():
         trip_duration_stats(df, user_filters)
         user_stats(df, user_filters)
 
+        display_filtered_data = input('\nWould you like to view your filtered data 5 lines at a time?\n'
+                                 "(Sounds like a boring way to view it, but I won't stop you!\n"
+                        'Enter yes or no.\n')
+        if display_filtered_data.lower() != 'yes':
+            # TODO: make this 5 lines at a time, rather than just head
+            print(df.head())
+
+        display_raw_data = input('\nWould you like to view the whole dataset for your chosen city 5 lines at a time?\n'
+                                 "(Sounds like a really boring way to view it, but I won't stop you! ;-)  )\n"
+                        'Enter yes or no.\n')
+        if display_raw_data.lower() != 'yes':
+            # TODO: make this 5 lines at a time, rather than just head
+            raw_df = pd.read_csv(CITY_DATA[user_filters['city']])
+            print(raw_df.head())
 
         restart = input('\nWould you like to restart to choose different filter options?\n'
                         'Enter yes or no.\n')
